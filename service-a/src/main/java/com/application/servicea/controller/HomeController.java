51647a1013b71304b235d04d3a24308d222a4417
hungph3 <hungph3@vnpay.vn>
2024-01-29 16:58:53 +0700

    Initial

---
+++ service-a/src/main/java/com/application/servicea/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.application.servicea.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @Value("${health-check.message}")
+    private String healthCheckMessage;
+
+    @GetMapping("/healthz")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok(healthCheckMessage);
+    }
+}
